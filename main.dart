134a1538ea0cb5b5db1c5772acacfb751304c05f
ahsan ali <57295435+AhsanAliProgrammer@users.noreply.github.com>
2022-11-02 20:53:14 +0000

    assingment 3

---
+++ main.dart
@@ -0,0 +1,50 @@
+void main() {
+  // List names=["adil","aqib","ahsan"] ;
+
+  //   print(names[0]);
+  //   print(names[1]);
+  //   print(names[2]);
+
+  // question 2
+
+  // List days=[];
+  // days.add("Sunday");
+
+  // days.add("Monday");
+
+  // days.add("Tuesday");
+
+  // days.add("Wednesday");
+
+  // days.add("Thursday");
+
+  // days.add("Friday");
+
+  // days.add("Saturday");
+  // print(days);
+
+  // question 3
+
+  // List friends=["aqib","wajahat","ahsan","yaseen"];
+  // print(friends.where((friends)=>friends.substring(0,1).contains('a')));
+
+  //question 4
+
+  // Map person={'name':"adil","adress":"FB-Area","city":"karachi","country":"Australia"};
+  // person.update('country',(value) =>"Pakistan");
+  // print(person);
+
+  // question 5
+
+  // Map person={"Name":"Adil","phone":60444};
+  // person.keys.where((person)=>person.length==4);
+
+  // question 6
+
+  //  Map world={ "countries":[{"name":"Pakistan","Capital":"Islamabad","Currency":"PKR",
+  // "Language":"Urdu"},
+  // {"name":"India","Capital":"Dehli","Currency":"INDR",
+  //  "Language":"Hindi"}]};
+  //  world.forEach((k,v) => print('${k}: ${v}'));
+  echo "hellow";
+}
